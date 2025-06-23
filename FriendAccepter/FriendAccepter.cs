dae10d97a9eff16ff093c582d984dff1c23ed9e5
Jackie Ryan <jackiewaltryan@gmail.com>
2025-06-23 13:38:39 +0300

    Update FriendAccepter.cs

--- FriendAccepter/FriendAccepter.cs
+++ FriendAccepter/FriendAccepter.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
 using System.Threading.Tasks;
 using ArchiSteamFarm.Helpers.Json;
 using ArchiSteamFarm.Plugins.Interfaces;
 using ArchiSteamFarm.Steam;
+using ArchiSteamFarm.Steam.Integration;
+using ArchiSteamFarm.Web.Responses;
 
 namespace FriendAccepter;
 
@@ -23,11 +26,21 @@ internal sealed class AutoPostConfig {
     internal AutoPostConfig() { }
 }
 
+internal sealed class AddGroupCommentResponse {
+    [JsonInclude]
+    [JsonPropertyName("success")]
+    internal bool Success { get; }
+
+    [JsonConstructor]
+    internal AddGroupCommentResponse() { }
+}
+
 internal sealed class FriendAccepter : IGitHubPluginUpdates, IBotModules, IBotFriendRequest {
     public string Name => nameof(FriendAccepter);
     public string RepositoryName => "JackieWaltRyan/FriendAccepter";
     public Version Version => typeof(FriendAccepter).Assembly.GetName().Version ?? throw new InvalidOperationException(nameof(Version));
     public Dictionary<string, bool> Bots = new();
+    public Dictionary<string, Timer> BotsTimers = new();
 
     public Task OnLoaded() => Task.CompletedTask;
 
@@ -95,20 +108,37 @@ internal sealed class FriendAccepter : IGitHubPluginUpdates, IBotModules, IBotFr
     }
 
     // ReSharper disable once FunctionRecursiveOnAllPaths
-    public static async Task AutoPost(Bot bot, AutoPostConfig config) {
-        try {
-            if (!bot.IsConnectedAndLoggedOn) {
-                await Task.Delay(TimeSpan.FromMinutes(1)).ConfigureAwait(false);
-                await AutoPost(bot, config).ConfigureAwait(false);
+    public async Task AutoPost(Bot bot, AutoPostConfig config) {
+        if (!bot.IsConnectedAndLoggedOn) {
+            // ReSharper disable once AsyncVoidLambda
+            // ReSharper disable once UnusedParameter.Local
+            BotsTimers[bot.BotName] = new Timer(async e => await AutoPost(bot, config).ConfigureAwait(false), null, TimeSpan.FromMinutes(1), TimeSpan.FromMicroseconds(-1));
+        }
+
+        ObjectResponse<AddGroupCommentResponse>? rawResponse = await bot.ArchiWebHandler.UrlPostToJsonObjectWithSession<AddGroupCommentResponse>(
+            new Uri(ArchiWebHandler.SteamCommunityURL, $"/comment/Clan/post/{config.GroupID}/-1/"), data: new Dictionary<string, string>(4) {
+                { "comment", config.Comment },
+                { "count", "10" },
+                { "feature2", "-1" }
             }
+        ).ConfigureAwait(false);
 
-            bot.ArchiLogger.LogGenericInfo("AutoPost is run.");
+        AddGroupCommentResponse? response = rawResponse?.Content;
 
-            await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
-            await AutoPost(bot, config).ConfigureAwait(false);
-        } catch {
-            await Task.Delay(TimeSpan.FromMinutes(1)).ConfigureAwait(false);
-            await AutoPost(bot, config).ConfigureAwait(false);
+        if (response == null) {
+            // ReSharper disable once AsyncVoidLambda
+            // ReSharper disable once UnusedParameter.Local
+            BotsTimers[bot.BotName] = new Timer(async e => await AutoPost(bot, config).ConfigureAwait(false), null, TimeSpan.FromMinutes(1), TimeSpan.FromMicroseconds(-1));
+        } else {
+            bot.ArchiLogger.LogGenericInfo($"Add comment \"{config.Comment}\" to group {config.GroupID} return status {response.Success}.");
+
+            uint timeout = response.Success ? config.Timeout : 1;
+
+            bot.ArchiLogger.LogGenericInfo($"Next send comment: {DateTime.Now.AddMinutes(timeout):T}");
+
+            // ReSharper disable once AsyncVoidLambda
+            // ReSharper disable once UnusedParameter.Local
+            BotsTimers[bot.BotName] = new Timer(async e => await AutoPost(bot, config).ConfigureAwait(false), null, TimeSpan.FromMinutes(timeout), TimeSpan.FromMicroseconds(-1));
         }
     }
 }
