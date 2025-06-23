032cf3ed537d6ef49692fd59e1060f30170ad52c
Jackie Ryan <jackiewaltryan@gmail.com>
2025-06-23 23:27:05 +0300

    Test 3

--- FriendAccepter/FriendAccepter.cs
+++ FriendAccepter/FriendAccepter.cs
@@ -100,7 +100,7 @@ internal sealed class FriendAccepter : IGitHubPluginUpdates, IBotModules, IBotFr
                     { "comment", config.Comment },
                     { "count", "10" },
                     { "feature2", "-1" }
-                }, referer: new Uri(ArchiWebHandler.SteamCommunityURL, $"/groups/{config.GroupID}/comments"), session: ArchiWebHandler.ESession.Lowercase
+                }, referer: new Uri(ArchiWebHandler.SteamCommunityURL, $"/groups/{config.GroupID}/comments")
             ).ConfigureAwait(false);
 
             JsonElement? response = rawResponse?.Content;
